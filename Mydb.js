7db79b87f9d716a417865f791dbc0ca5551e8766
Jit Chakraborty <159631656+fabulousman12@users.noreply.github.com>
2025-05-29 19:04:38 +0530

    Add files via upload
    CHAnge databses name in env

--- Mydb.js
+++ Mydb.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 // Create a MySQL connection pool
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOSTDEV,       // Replace with your MySQL host
-  user: process.env.MYSQLUSER,            // Replace with your MySQL username
-  password: process.env.MYSQLPASSWORDDEV,    // Replace with your MySQL password
-  database: process.env.DATABASEMYSQL2  ,
-  port: process.env.MYSQLPORT,
+  host: process.env.External_MySQL_HOST,       // Replace with your MySQL host
+  user: process.env.External_MySQL_USER,            // Replace with your MySQL username
+  password: process.env.External_MySQL_PASSWORD,    // Replace with your MySQL password
+  database: process.env.External_MySQL_DATABASE  ,
+  port: process.env.External_MySQL_PORT,
   
   // Replace with your MySQL database name
 });
